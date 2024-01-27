b4cb4dd18bfda7facd83dccde55653afb29825cf
Wesley Dunlap <wtdunlap@gmail.com>
2024-01-27 16:50:40 -0500

    Fleshing out ideas and questions for project

--- index.js
+++ index.js
@@ -5,33 +5,86 @@ inquirer
     .prompt([
         {
             type: "input",
-            message: "What is your name?",
-            name: "name",
+            message: "What is the title of your project?",
+            name: "title",
         },
         {
             type: "input",
-            message: "Where are you right now?",
-            name: "location",
+            message: "Please provide a breif description.",
+            name: "description",
         },
         {
             type: "input",
-            message: "Tell me a little about yourself?",
-            name: "biography",
+            message: "How is your project supposed to be used?",
+            name: "usage",
         },
         {
             type: "input",
-            message: "What's your linkedin?",
-            name: "linkedin",
+            message:
+                "Please provide three (3) features, one at a time. This is feature one (1)",
+            name: "feature1",
         },
         {
             type: "input",
-            message: "What's your github?",
-            name: "github",
+            message:
+                "Please provide three (3) features, one at a time. This is feature two (2)",
+            name: "feature2",
+        },
+        {
+            type: "input",
+            message:
+                "Please provide three (3) features, one at a time. This is feature three (3)",
+            name: "feature3",
+        },
+        {
+            type: "input",
+            message: "please provide the URL to the deployed site.",
+            name: "deploy",
+        },
+        {
+            type: "input",
+            message: "Please provide the URL to the repo for your project.",
+            name: "repo",
+        },
+        {
+            type: "input",
+            message:
+                "Please provide details on anyone that worked on this project in the form of a paragraph.",
+            name: "credits",
         },
     ])
     .then((response) => {
-        const body = `README.md BODY GOES HERE WITH FORMATTING`;
-        fs.writeFile("./results/README.md", body, (err) =>
-            err ? console.error(err) : console.log("Success!")
-        );
+        const body = `
+
+        # ${response.title}
+
+        ## Description
+
+        ${response.description}
+
+        ## Usage
+
+        ${response.usage}
+
+        ## Features
+
+        - ${response.feature1}
+        - ${response.feature2}
+        - ${response.feature3}
+
+        ## Deployment and Repo
+
+        ${response.deploy}
+
+        ${response.repo}
+
+        ## Credits
+
+        ${response.credits}
+
+        `;
     });
+
+fs.writeFile("./results/README.md", body, (err) =>
+    err ? console.error(err) : console.log("Success!")
+);
